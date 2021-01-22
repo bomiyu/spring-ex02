b43242618741490e4ab9fe943afb1fa76900a3c0
User <bom7768@gmail.com>
2021-01-22 14:29:09 +0900

    2021 01 22

--- src/main/java/org/zerock/domain/Criteria.java
+++ src/main/java/org/zerock/domain/Criteria.java
@@ -8,18 +8,33 @@ import lombok.ToString;
 @Setter
 @ToString
 public class Criteria {
-	
+
 	private int pageNum;
 	private int amount;
 	
+	private String type;
+	private String keyword;
 	
 	public Criteria() {
 		this(1, 10);
 	}
-	
+
 	public Criteria(int pageNum, int amount) {
 		this.pageNum = pageNum;
 		this.amount = amount;
-		
+	}
+	
+	public String[] getTypeArr() {
+		if (this.type == null) {
+			return new String[] {};
+		} else {
+			return type.split("");
+		}
 	}
 }
+
+
+
+
+
+
