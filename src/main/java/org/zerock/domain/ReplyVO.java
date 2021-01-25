8823c6dd4602a3c82ca6ed5f542a1cac193324fb
User <bom7768@gmail.com>
2021-01-25 09:43:00 +0900

    2021 01 25

---
+++ src/main/java/org/zerock/domain/ReplyVO.java
@@ -0,0 +1,14 @@
+package org.zerock.domain;
+
+import java.util.Date;
+
+public class ReplyVO {
+	private Long rno;
+	private Long bno;
+	
+	private String reply;
+	private String replyer;
+	private Date replyDate;
+	private Date updateDate;
+
+}
