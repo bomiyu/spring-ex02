d392e734c987a430f56ac84d8cf182aec9161d57
User <bom7768@gmail.com>
2021-02-01 18:48:01 +0900

    2021 02 01

--- src/main/java/org/zerock/mapper/BoardMapper.java
+++ src/main/java/org/zerock/mapper/BoardMapper.java
@@ -2,16 +2,17 @@ package org.zerock.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
 import org.zerock.domain.BoardVO;
 import org.zerock.domain.Criteria;
 
 public interface BoardMapper {
 	
-
 	public int getTotalCount(Criteria cri);
-	//SELECT count(*) FROM tbl_board
+	// SELECT count(*) FROM tbl_board
 	
 	public List<BoardVO> getList();
+	
 	public List<BoardVO> getListWithPaging(Criteria cri);
 	
 	public void insert(BoardVO board);
@@ -34,16 +35,21 @@ public interface BoardMapper {
 	*/
 	
 	public void insertSelectKey(BoardVO board);
+	// 1. seq_board의 nextval을 먼저 조회(select)
+	// 2. 조회된 nextval을 insert에서 사용
 	
-		//1.seq_board의 nextval을 먼저 조히(select)
-		//2. 조회된 nextval을 insert에서 사용
 	
 	public BoardVO read(Long bno);
 	
 	public int delete(Long bno);
+	
 	public int update(BoardVO board);
-	public int modify(BoardVO board);
-
-
-
+	
+	public void updateReplyCnt(@Param("bno") Long bno, @Param("amount") int amount);
 }
+
+
+
+
+
+
